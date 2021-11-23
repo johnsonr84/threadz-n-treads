aa61bae29c04f48ffbc8383208c04fac3f3547b5
Your Name <robertcjohnson1984@gmail.com>
2021-11-23 15:25:38 -0700

    update return

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,7 @@
 export default function IndexPage() {
     return (
         <>
-            <p>Hello!</p>
-            <code>var</code>
-            <p>I am a child</p>
+            <p>Hey!</p>
         </>
     );
 }
