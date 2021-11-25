00b769a30de21f735f04a821791f7efe10dba475
Your Name <robertcjohnson1984@gmail.com>
2021-11-25 13:07:28 -0700

    create Cart, add cart query to User and calcTotalPrice

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import styled from 'styled-components';
+import Cart from './Cart';
 import Nav from './Nav';
 
 const Logo = styled.h1`
@@ -38,13 +39,14 @@ export default function Header() {
     <HeaderStyles>
       <div className="bar">
         <Logo>
-          <Link href="/">Threadz N' Treads</Link>
+          <Link href="/">Sick fits</Link>
         </Logo>
+        <Nav />
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
-      <Nav />
+      <Cart />
     </HeaderStyles>
   );
 }
