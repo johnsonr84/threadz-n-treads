cbf8089682694c4837e7c040f498b297de1c3ee8
Your Name <robertcjohnson1984@gmail.com>
2021-11-25 12:48:33 -0700

    insert seed data

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,54 +8,66 @@ import { ProductImage } from './schemas/ProductImage';
 import { Product } from './schemas/Product';
 import { User } from './schemas/User';
 import 'dotenv/config';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-threadz-n-threads';
 
-const sessionConfig = {
-  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
-  secret: process.env.COOKIE_SECRET,
-};
-
-const { withAuth } = createAuth({
-  listKey: 'User',
-  identityField: 'email',
-  secretField: 'password',
-  initFirstItem: {
-    fields: ['name', 'email', 'password'],
-    // TODO: Add in inital roles here
-  },
-});
-
-export default withAuth(
-  config({
-    // @ts-ignore
-    server: {
-      cors: {
-        origin: [process.env.FRONTEND_URL],
-        credentials: true,
+  const sessionConfig = {
+    maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+    secret: process.env.COOKIE_SECRET,
+  };
+  
+  const { withAuth } = createAuth({
+    listKey: 'User',
+    identityField: 'email',
+    secretField: 'password',
+    initFirstItem: {
+      fields: ['name', 'email', 'password'],
+      // TODO: Add in inital roles here
+    },
+    passwordResetLink: {
+      async sendToken(args) {
+        console.log(args);
       },
     },
-    db: {
-      adapter: 'mongoose',
-      url: databaseURL,
-      // TODO: Add data seeding here
-    },
-    lists: createSchema({
-      // Schema items go in here
-      User,
-      Product,
-      ProductImage,
-    }),
-    ui: {
-      // Show the UI only for poeple who pass this test
-      isAccessAllowed: ({ session }) =>
-        // console.log(session);
-        !!session?.data,
-    },
-    session: withItemData(statelessSessions(sessionConfig), {
-      // GraphQL Query
-      User: 'id name email',
-    }),
-  })
-);
+  });
+  
+  export default withAuth(
+    config({
+      // @ts-ignore
+      server: {
+        cors: {
+          origin: [process.env.FRONTEND_URL],
+          credentials: true,
+        },
+      },
+      db: {
+        adapter: 'mongoose',
+        url: databaseURL,
+        async onConnect(keystone) {
+          console.log('Connected to the database!');
+          if (process.argv.includes('--seed-data')) {
+            await insertSeedData(keystone);
+          }
+        },
+      },
+      lists: createSchema({
+        // Schema items go in here
+        User,
+        Product,
+        ProductImage,
+      }),
+      ui: {
+        // Show the UI only for poeple who pass this test
+        isAccessAllowed: ({ session }) =>
+          // console.log(session);
+          !!session?.data,
+      },
+      session: withItemData(statelessSessions(sessionConfig), {
+        // GraphQL Query
+        User: 'id name email',
+      }),
+    })
+  );
+  
