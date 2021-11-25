d14d83ba56be7bf9f4e7dde86901d69674e30eb8
Your Name <robertcjohnson1984@gmail.com>
2021-11-25 14:05:00 -0700

    add accessArgs and rules

--- backend/access.ts
+++ backend/access.ts
@@ -15,9 +15,30 @@ const generatedPermissions = Object.fromEntries(
   ])
 );
 
+// Permissions check if someone meets a criteria - yes or no.
 export const permissions = {
   ...generatedPermissions,
   isAwesome({ session }: ListAccessArgs): boolean {
     return session?.data.name.includes('wes');
+  },
+};
+
+// Rule based function
+// Rules can return a boolean - yes or no - or a filter which limits which products they can CRUD.
+export const rules = {
+  canManageProducts({ session }: ListAccessArgs) {
+    // 1. Do they have the permission of canManageProducts
+    if (permissions.canManageProducts({ session })) {
+      return true;
+    }
+    // 2. If not, do they own this item?
+    return { user: { id: session.itemId } };
+  },
+  canReadProducts({ session }: ListAccessArgs) {
+    if (permissions.canManageProducts({ session })) {
+      return true; // They can read everything!
+    }
+    // They should only see available products (based on the status field)
+    return { status: 'AVAILABLE' };
   },
 };
