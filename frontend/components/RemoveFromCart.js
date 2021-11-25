6a64597080320e53526c07120e2aaee9a3137c6e
Your Name <robertcjohnson1984@gmail.com>
2021-11-25 13:25:53 -0700

    update remcart

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -20,9 +20,20 @@ const REMOVE_FROM_CART_MUTATION = gql`
   }
 `;
 
+function update(cache, payload) {
+  cache.evict(cache.identify(payload.data.deleteCartItem));
+}
+
 export default function RemoveFromCart({ id }) {
   const [removeFromCart, { loading }] = useMutation(REMOVE_FROM_CART_MUTATION, {
     variables: { id },
+    update,
+    // optimisticResponse: {
+    //   deleteCartItem: {
+    //     __typename: 'CartItem',
+    //     id,
+    //   },
+    // },
   });
   return (
     <BigButton
