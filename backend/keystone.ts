fd400c30e15d6cb5d77477e54c16584ef8fb28b7
Your Name <robertcjohnson1984@gmail.com>
2021-12-13 23:45:08 -0700

    update to keystone-6

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -1,9 +1,6 @@
-import { createAuth } from '@keystone-next/auth';
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import {
-  withItemData,
-  statelessSessions,
-} from '@keystone-next/keystone/session';
+import { createAuth } from '@keystone-6/auth';
+import { config } from '@keystone-6/core';
+import { statelessSessions } from '@keystone-6/core/session';
 import { permissionsList } from './schemas/fields';
 import { Role } from './schemas/Role';
 import { OrderItem } from './schemas/OrderItem';
@@ -17,73 +14,64 @@ import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
 import { extendGraphqlSchema } from './mutations';
 
+const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-threadz-n-threads';
+// const databaseURL = process.env.DATABASE_URL || 'file:./keystone.db';
 
-function check(name: string) {}
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET || 'this secret should only be used in testing',
+};
 
-const databaseURL =
-  process.env.DATABASE_URL || 'mongodb://localhost/keystone-threadz-n-threads';
-
-  const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
-    secret: process.env.COOKIE_SECRET,
-  };
-  
-  const { withAuth } = createAuth({
-    listKey: 'User',
-    identityField: 'email',
-    secretField: 'password',
-    initFirstItem: {
-      fields: ['name', 'email', 'password'],
-      // TODO: Add in inital roles here
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
     },
-    passwordResetLink: {
-      async sendToken(args) {
-        // send the email
-        await sendPasswordResetEmail(args.token, args.identity);
+  },
+  sessionData: `id name email role { ${permissionsList.join(' ')} }`,
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL!],
+        credentials: true,
       },
     },
-  });
-  
-  export default withAuth(
-    config({
-      // @ts-ignore
-      server: {
-        cors: {
-          origin: [process.env.FRONTEND_URL],
-          credentials: true,
-        },
+    db: {
+      provider: 'sqlite',
+      url: databaseURL,
+      async onConnect(context) {
+        console.log('Connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(context);
+        }
       },
-      db: {
-        adapter: 'mongoose',
-        url: databaseURL,
-        async onConnect(keystone) {
-          console.log('Connected to the database!');
-          if (process.argv.includes('--seed-data')) {
-            await insertSeedData(keystone);
-          }
-        },
-      },
-      lists: createSchema({
-        // Schema items go in here
-        User,
-        Product,
-        ProductImage,
-        CartItem,
-        OrderItem,
-        Order,
-        Role,
-      }),
-      extendGraphqlSchema,
-      ui: {
-        // Show the UI only for poeple who pass this test
-        isAccessAllowed: ({ session }) =>
-          // console.log(session);
-          !!session?.data,
-      },
-      session: withItemData(statelessSessions(sessionConfig), {
-        // GraphQL Query
-        User: `id name email role { ${permissionsList.join(' ')} }`,
-      }),
-    })
-  );
-  
+    },
+    lists: {
+      // Schema items go in here
+      User,
+      Product,
+      ProductImage,
+      CartItem,
+      OrderItem,
+      Order,
+      Role,
+    },
+    extendGraphqlSchema,
+    ui: {
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) => !!session,
+    },
+    session: statelessSessions(sessionConfig),
+  })
+);
