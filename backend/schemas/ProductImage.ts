3017b383964f06681a205def38f41be00b3de32d
Your Name <robertcjohnson1984@gmail.com>
2021-12-14 01:04:06 -0700

    update to keystone-6/core

--- backend/schemas/ProductImage.ts
+++ backend/schemas/ProductImage.ts
@@ -1,16 +1,25 @@
 import 'dotenv/config';
-import { relationship, text } from '@keystone-next/fields';
-import { list } from '@keystone-next/keystone/schema';
-import { cloudinaryImage } from '@keystone-next/cloudinary';
+import { relationship, text } from '@keystone-6/core/fields';
+import { list } from '@keystone-6/core';
+import { cloudinaryImage } from '@keystone-6/cloudinary';
+import { isSignedIn, permissions } from '../access';
 
 export const cloudinary = {
-  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
-  apiKey: process.env.CLOUDINARY_KEY,
-  apiSecret: process.env.CLOUDINARY_SECRET,
-  folder: 'threadznthreads',
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME || 'fake',
+  apiKey: process.env.CLOUDINARY_KEY || 'fake',
+  apiSecret: process.env.CLOUDINARY_SECRET || 'fake',
+  folder: 'threadzntreads',
 };
 
 export const ProductImage = list({
+  access: {
+    operation: {
+      create: isSignedIn,
+      query: () => true,
+      update: permissions.canManageProducts,
+      delete: permissions.canManageProducts,
+    },
+  },
   fields: {
     image: cloudinaryImage({
       cloudinary,
