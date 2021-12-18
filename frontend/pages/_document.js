d56d9073f81edae7cf333169eadc0f16fd5c0c48
Your Name <robertcjohnson1984@gmail.com>
2021-12-17 23:55:16 -0700

    update document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -2,24 +2,24 @@ import Document, { Html, Head, NextScript, Main } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-    static getInitialProps({ renderPage }) {
-        const sheet = new ServerStyleSheet();
-        const page = renderPage((App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        );
-        const styleTags = sheet.getStyleElement();
-        return { ...page, styleTags };
-    }
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
 
-    render() {
-        return (
-            <Html lang="en-CA">
-                <Head />
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </Html>
-        );
-    }
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
